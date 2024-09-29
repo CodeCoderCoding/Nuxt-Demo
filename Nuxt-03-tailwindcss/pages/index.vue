f526202da2000da94626528b3cec14648959d0aa
Neil <1845371176@qq.com>
2024-09-29 22:36:09 +0800

    [feat] add Nuxt-tailwindcss-demo

---
+++ Nuxt-03-tailwindcss/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-full flex p-96 justify-center items-center">
+    <div class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Hello Tailwind CSS</div>
+  </div>
+</template>
