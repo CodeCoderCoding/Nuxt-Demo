307b4b5b26c5ebbc7d1e5c6636c842e33510b55b
Neil <1845371176@qq.com>
2024-10-12 10:43:12 +0800

    [feat] add Nuxt-Element-Plus-Starter

---
+++ Nuxt-23-Element-Plus-Starter/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-index">
+    <Logos />
+    <Examples />
+  </div>
+</template>
+
+<style>
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import Tree from "~/components/Tree.vue";
+</script>
