cf3f505ac6d0bea3e439af6994a1818a30a2a0de
Neil <1845371176@qq.com>
2024-10-12 10:38:23 +0800

    [feat] add Nuxt-Element-Plus-demo

---
+++ Nuxt-22-Element-Plus-Demo/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.common-layout {
+  display: flex;
+  flex-direction: column;
+
+  .el-header,
+  .el-footer,
+  .el-main,
+  .el-aside {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .el-header,
+  .el-footer {
+    background-color: #9cb7ce;
+    color: black;
+    text-align: center;
+  }
+
+  .el-aside {
+    background-color: #799bb9;
+    color: black;
+    text-align: center;
+  }
+
+  .el-main {
+    background-color: aliceblue;
+    color: black;
+    text-align: center;
+    height: 150px;
+  }
+}
+</style>
